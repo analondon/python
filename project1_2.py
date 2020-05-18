b0d9d659847ec4618c1ba6e24be632feb6795eb0
analondon <58117312+analondon@users.noreply.github.com>
2020-05-18 21:58:06 +0100

    Project Part 2

--- project1_2.py
+++ project1_2.py
@@ -1,6 +1,11 @@
 import msvcrt as m
 import re
 
+def backtomain(): #escape not working
+    escape = input('Project not found. Press <ESCAPE> to go back to the Main Menu or <ENTER> to try again: ', )
+    if m.kbhit() and m.getch()[0] == 27:
+        main_menu()
+
 all_projects = {}
 
 def main_menu():
@@ -15,13 +20,13 @@ def main_menu():
 
     """
     )
-
+#gets input from user, converts into uppercase to be compared with the option in condfun()
 def actioninput():
     actioninput.action = input('            Please choose an option and press <Enter>: ').upper();
 
-
+#parent class
 class Project:
-
+    #allowes our user to add a new project into the file
     def func_add_project():
         global project_name
         global nameofproject
@@ -31,15 +36,14 @@ class Project:
 
         while True:
             project_name = input('    Enter the project name: ')
-            if not re.match('[a-zA-Z0-9]', project_name):
+            if not re.match('[a-zA-Z0-9]', project_name): #checks in the name is valid
                 print('Name must have letters and numbers')
                 continue
             else:
                 if not project_name in all_projects :
-                    nameofproject = project_name
-                    all_projects[project_name] = {}
-                    print(all_projects)
-                    return nameofproject
+                    nameofproject = project_name #project_name will be turned into a dictionary named after the input from the user. String from input saved in a new variable to be accessed later in the code
+                    all_projects[project_name] = {} #addsthe new project created into the main dictionary of projects
+                    return nameofproject #returns string from user input
                 else:
                     print('Name already exists. Please enter a new name.')
                     continue
@@ -49,41 +53,47 @@ class Project:
         while True:
             action_v = input('    Enter the project name: ')
             if not action_v in all_projects.keys():
-                print('Project not found')
+                backtomain() #will go back to the main menu if project name does not exist so it can be looked up or added
                 continue
             else:
                 print(f"There are {all_projects[nameofproject]['Number_of_members']} team members")
                 break
 
-        names_voters = temp_list
-        allvotes = []
+        names_voters = temp_list #temp list is created in class Person. A copy is made for looping thro
         while True:
             for nv in names_voters:
                 total = 0
                 vote = 0
 
-                print(f"Enter {nv}'s votes, points must add up to 100: ")
+                print(f"""Enter {nv}'s votes, points must add up to 100: """)
                 for k, v in all_projects[nameofproject]['Members'].items():
                     if nv != k:
-                        vote = input(f"       Enter {nv}'s points for {k}: ")
-                        total = total + int(vote)
-                        all_projects[nameofproject]['Members'][k] = v + int(vote)
-                        print(all_projects, 2)
-                        print(k, v, 3)
+                        vote = input(f"""
+                               Enter {nv}'s points for {k}:""")
+                        if not re.match('[0-9]', vote):
+                            print('Points should be a valid number in between 0 and 100. Please press <ENTER> and start again.')
+                            break
+                        else:
+                            total = total + int(vote)
+                            all_projects[nameofproject]['Members'][k] = v + int(vote)
+                            continue
 
-                if total != 100 :
+                if re.match('[0-9]', vote) and total != 100 :
                     print('Points must add up to 100. Please start again.')
-                    continue
+                    break
             break
 
         print(input('Press <Enter> to return to the main menu: ', ));
 
+#child class
 class Person(Project):
     def func_add_members():
         global temp_list
         while True:
             number_of_members = input('Enter the number of team members: ')
-            if not re.match('[0-9]', number_of_members):
+            try:
+                int(number_of_members)
+            except:
                 print('<ERROR> It must be a number greater than 0')
                 continue
             else:
@@ -110,17 +120,14 @@ class Person(Project):
         all_projects[nameofproject]['Members'] = members_names
         proj_members = {n : 0 for n in all_projects[nameofproject]['Members']}
         all_projects[nameofproject]['Members'] = proj_members
-        print(proj_members, 'XXX')
 
-        # print(members_names, '3')
-        print(all_projects)
         print(input('Press <Enter> to return to the main menu: ', ));
 
 def func_a():
     print('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
 def func_error():
-    print('<ERROR>');
+    print('<ERROR> Please choose another option');
 
 def condfun():
     if actioninput.action == 'A' :
