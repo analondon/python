47962df06c08a882103829d10bb426d5797a0b2d
analondon <58117312+analondon@users.noreply.github.com>
2020-05-25 21:47:59 +0100

    Utils

---
+++ Utils.py
@@ -0,0 +1,22 @@
+import Classes
+from Classes import Project, Person
+
+
+#other functions
+def backtomain(): #escape not working
+    if m.kbhit() and m.getch()[0] == 27: #not working
+        self.main_menu()
+
+def checkuserinput():
+    #check if input is valid. It will be adaptable according to the use
+    pass
+
+def func_error():
+    print("""
+            <ERROR> Please choose another option""");
+
+def func_a():
+    print("""
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labor et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi uts aliquip ex ea commodo consequat.
+
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cil dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa officia deserunt mollit anim id est laborum.""");
